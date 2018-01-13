58e2440d2a623e5e11cfe0fd2e075b56ff8e4746
Dave Dubin <ddubin@illinois.edu>
2018-01-13 16:42:13 -0600

    assignments on syllabus

--- NewCalendar2.py
+++ NewCalendar2.py
@@ -19,7 +19,7 @@ if project:
         defsfilename = project + ".defs"
         defsfile = open(defsfilename,"w")
 
-l561 = Namespace("http://courseweb.ischool.illinois.edu/lis/2017sp/lis561/")
+i561 = Namespace("http://courseweb.ischool.illinois.edu/is561/")
 event = Namespace("http://purl.org/NET/c4dm/event.owl#")
 tl = Namespace("http://purl.org/NET/c4dm/timeline.owl#")
 dc = Namespace("http://purl.org/dc/terms/")
@@ -31,24 +31,24 @@ mygraph.parse(ttlfilename,format="n3")
 wlist = []  # List of weeks
 weekstart = {} # associate weeks with their starting date
 
-for s in mygraph.subjects(RDF.type, l561.Week):
+for s in mygraph.subjects(RDF.type, i561.Week):
 	for i in mygraph.objects(s,event.time):
 	        for a in mygraph.objects(i,tl.at):
 			    weekstart[str(a)] = s
 deadlines = {}
 
-for d in mygraph.subjects(RDF.type, l561.Deadline):
+for d in mygraph.subjects(RDF.type, i561.Deadline):
                             weekdue = duedate = adue = dlabel = alabel = ""
                             for o in mygraph.objects(d,RDFS.label):
                                     dlabel = str(o)
-                            for o in mygraph.objects(d,l561.dueDuring):
+                            for o in mygraph.objects(d,i561.during):
                                     weekdue = o
-                            for o in mygraph.objects(d,l561.dueDate):
+                            for o in mygraph.objects(d,i561.date):
                                     duedate = str(o)
-                            for a in mygraph.subjects(l561.hasDeadline,d):
+                            for a in mygraph.subjects(i561.hasDeadline,d):
                                     for o in mygraph.objects(a,RDFS.label):
                                             alabel = str(o)
-                                    for o in mygraph.objects(a,l561.dueDate):
+                                    for o in mygraph.objects(a,i561.dueDate):
                                             adue = str(o)
                             if (weekdue in deadlines.keys()):
                                     deadlines[weekdue] += "**Due:** " + dlabel + ", " + alabel + "\n\n"
@@ -65,16 +65,19 @@ for d in wlist:
         myweek = myconcept = required = background  = ''
 	for o in mygraph.objects(weekstart[d], RDFS.label):
 	      myweek = str(o)
-	for o in mygraph.objects(weekstart[d], l561.date):
+	for o in mygraph.objects(weekstart[d], i561.date):
 	      weekdate = str(o)
         cldrfile.write("\n")
-        cldrfile.write("### " +  myweek + ": " + weekdate + "\n")
+        cldrfile.write("### " +  myweek + ": " + weekdate + "\n\n")
+        if weekstart[d] in deadlines.keys():
+               cldrfile.write(dstring + "" + deadlines[weekstart[d]] + "\n")
+               cldrfile.write("\n")
 	for s in mygraph.objects(weekstart[d],dc.subject):
 	      for p in mygraph.objects(s,skos.prefLabel):
                       myconcept = str(p)
-                      for q in mygraph.objects(s,l561.backgroundReading):
+                      for q in mygraph.objects(s,i561.backgroundReading):
                               background = str(q)
-                      for r in mygraph.objects(s,l561.reqReading):
+                      for r in mygraph.objects(s,i561.reqReading):
                               required = str(r)
               cldrfile.write("- " + myconcept + "\n")  
               rstring = "    - **Required Readings:** "
@@ -82,9 +85,6 @@ for d in wlist:
                 cldrfile.write(rstring + " " + required + "\n")
                 cldrfile.write("\n")
               dstring = ""
-              if weekstart[d] in deadlines.keys():
-                     cldrfile.write(dstring + " " + deadlines[weekstart[d]] + "\n")
-                     cldrfile.write("\n")
               bstring = "    - **Further Background:** "
               if background:
                 cldrfile.write(bstring + " " + background + "\n")
---
+++ SecBSpring18.m4
@@ -0,0 +1,37 @@
+include(Spring18Readings.m4)dnl
+include(SecBSpring18.defs)dnl
+define(MYSECDATA, Spring18Bdata.md)dnl
+define(COURSETITLE, Information Modeling)dnl
+define(ASSIGNMENTS, Assignments.md)dnl
+define(CALENDAR, SecBSpring18.cldr)dnl
+define(OVERVIEW, OverviewObjectives.md)dnl
+define(METADATA, syllabib.md)dnl
+define(SAFETY, RHFshort.md)dnl
+define(CONTACT1, Schneider.md)dnl
+define(CONTACT2, Jett.md)dnl
+define(CONTACT3, KLee.md)dnl
+define(CONTACT4, LoLee.md)dnl
+define(CONTACT5, Empty.md)dnl
+define(TA1, AboutJett.md)dnl
+define(TA2, AboutKLee.md)dnl
+define(TA3, AboutLoLee.md)dnl
+define(INSTRUCTOR, AboutSchneider.md)dnl
+define(QUIZDUEDAY, Friday)dnl
+define(FINALSEC, # Readings)dnl
+define(PRES0DATE, Week before class)dnl
+define(PRES1DATE, January 16 and 18)dnl
+define(PRES2DATE, January 23 and 25)dnl
+define(PRES3DATE, January 30 and February 1)dnl
+define(PRES4DATE, February 6 and 8)dnl
+define(PRES5DATE, February 13 and 15)dnl
+define(PRES6DATE, February 20 and 22)dnl
+define(PRES7DATE, February 27 and March 1)dnl
+define(PRES8DATE, March 6 and 8)dnl
+define(PRES9DATE, March 13 and 15)dnl
+define(PRES10DATE, March 20 and 22)dnl
+define(PRES11DATE, March 27 and 29)dnl
+define(PRES12DATE, April 3 and 5)dnl
+define(PRES13DATE, April 10 and 13)dnl
+define(PRES14DATE, April 17 and 19)dnl
+define(PRES15DATE, April 24 and 26)dnl
+define(PRES16DATE, May 1)dnl
